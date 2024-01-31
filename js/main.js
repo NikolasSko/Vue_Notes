b5df4f737fbae8ad0503e56528a2c2bebd85a35c
NikolasSko <Skobelev.nikita12345@mail.ru>
2024-01-31 15:27:33 +0700

    Добавление и удаление заметок

--- js/main.js
+++ js/main.js
@@ -2,6 +2,35 @@ let app = new Vue({
     el: '#app',
     data: {
         logo: "./assets/лого.svg",
-        header: "Notes"
+        header: "Notes",
+        currentNote: "",
+        notes: [
+            {
+                text: 'Помыть посуду',
+                isCompleted: true
+            },
+            {
+                text: 'Постирать вещи',
+                isCompleted: true
+            },
+            {
+                text: 'Погладить вещи',
+                isCompleted: false
+            }
+        ],
+    },
+    methods:{
+        addNote: function(){
+            this.notes.push({
+                text: this.currentNote,
+                isCompleted: false
+            });
+            this.currentNote = "";
+        },
+        deleteNote: function(noteText){
+            this.notes =this.notes.filter(note =>{
+                return note.text !== noteText;
+            })
+        }
     }
 })
